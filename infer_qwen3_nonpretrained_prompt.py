5e5649a6077d517bc9b52b5c2f607812b3e09732
Qiu Jun <qiujun2004@outlook.com>
2025-05-22 07:51:41 +0800

    feat: refactor infer_qwen3_nonpretrained_prompt.py for improved structure and clarity

--- infer_qwen3_nonpretrained_prompt.py
+++ infer_qwen3_nonpretrained_prompt.py
@@ -1,9 +1,17 @@
 import json
-from modelscope import snapshot_download
 import torch
 from tqdm import tqdm
-from modelscope import AutoTokenizer
+from modelscope import snapshot_download, AutoTokenizer
 from transformers import AutoModelForCausalLM
+
+
+system_prompt_path = "./prompt/infer_qwen3_nonpretrained_prompt.txt"
+test_json_new_path = "test.json"
+
+model_dir = snapshot_download("Qwen/Qwen3-0.6B", cache_dir="./", revision="master")
+
+tokenizer = AutoTokenizer.from_pretrained("./Qwen/Qwen3-0.6B/", use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("./Qwen/Qwen3-0.6B/", device_map="auto", torch_dtype=torch.bfloat16)
 
 def predict(messages, model, tokenizer):
     device = "cuda"
@@ -21,21 +29,14 @@ def predict(messages, model, tokenizer):
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
     ]
-    
+
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-     
+
     return response
 
-model_dir = snapshot_download("Qwen/Qwen3-0.6B", cache_dir="./", revision="master")
-
-tokenizer = AutoTokenizer.from_pretrained("./Qwen/Qwen3-0.6B/", use_fast=False, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("./Qwen/Qwen3-0.6B/", device_map="auto", torch_dtype=torch.bfloat16)
-
-system_prompt_path = "./prompt/infer_qwen3_nonpretrained_prompt.txt"  # 你可以根据实际情况修改文件路径
 with open(system_prompt_path, 'r', encoding='utf-8') as prompt_file:
     system_prompt = prompt_file.read().strip()
 
-test_json_new_path = "test.json"
 with open(test_json_new_path, 'r', encoding='utf-8') as file:
     test_data = json.load(file)
 
@@ -43,7 +44,7 @@ with open("submit.csv", 'w', encoding='utf-8') as file:
     for idx, row in tqdm(enumerate(test_data)):
         input_value = row['question']
         id = row['id']
-        
+
         messages = [
             {"role": "system", "content": f"{system_prompt}"},
             {"role": "user", "content": f"{input_value}"}
