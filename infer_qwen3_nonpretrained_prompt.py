90fa39d9e53d881aeb576c813f3f424fdf0289df
Qiu Jun <qiujun2004@outlook.com>
2025-05-22 07:26:59 +0800

    feat: 添加Qwen3-0.6B原模型提示词测试脚本

---
+++ infer_qwen3_nonpretrained_prompt.py
@@ -0,0 +1,55 @@
+import json
+from modelscope import snapshot_download
+import torch
+from tqdm import tqdm
+from modelscope import AutoTokenizer
+from transformers import AutoModelForCausalLM
+
+def predict(messages, model, tokenizer):
+    device = "cuda"
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+    
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+     
+    return response
+
+model_dir = snapshot_download("Qwen/Qwen3-0.6B", cache_dir="./", revision="master")
+
+tokenizer = AutoTokenizer.from_pretrained("./Qwen/Qwen3-0.6B/", use_fast=False, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("./Qwen/Qwen3-0.6B/", device_map="auto", torch_dtype=torch.bfloat16)
+
+system_prompt_path = "./prompt/infer_qwen3_nonpretrained_prompt.txt"  # 你可以根据实际情况修改文件路径
+with open(system_prompt_path, 'r', encoding='utf-8') as prompt_file:
+    system_prompt = prompt_file.read().strip()
+
+test_json_new_path = "test.json"
+with open(test_json_new_path, 'r', encoding='utf-8') as file:
+    test_data = json.load(file)
+
+with open("submit.csv", 'w', encoding='utf-8') as file:
+    for idx, row in tqdm(enumerate(test_data)):
+        input_value = row['question']
+        id = row['id']
+        
+        messages = [
+            {"role": "system", "content": f"{system_prompt}"},
+            {"role": "user", "content": f"{input_value}"}
+        ]
+        response = predict(messages, model, tokenizer)
+        response = response.replace('\n', ' ')
+        file.write(f"{id},{response}\n")
+
+
